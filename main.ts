59723e6391f6c360c7dba01318b751d4f029f016
Patrick Coxall <mr.coxall@mths.ca>
2023-08-21 12:45:54 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-/* Created by: Mr. Coxall
+/* Copyright (c) 2020 MTHS All rights reserved
+ *
+ * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program ...
 */
 
 basic.showString('Hello, World!')
